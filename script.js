b03cc8b809fc4dc6bf02dc8f8bc0d61f43671bf7
CoderNawaki <50882239+CoderNawaki@users.noreply.github.com>
2023-12-08 23:51:49 +0900

    add smoth scroll feature to the page

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //add interactive features smooth scrolling ,form validation etc.
+
+document.addEventListener("DOMContentLoaded",function(){
+    const navLinks = document.querySelector('nav a');
+
+    navLinks.forEach(link=>{
+        link.addEventListener('click',smoothScroll);
+    });
+
+    function smoothScroll(e){
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top:targetElement.offsetTop-60,//adjust the offset if you have a fixed header
+            behavior:'smooth';
+        });
+    }
+});
