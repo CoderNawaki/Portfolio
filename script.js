57254a239f5fc9fa11903af0a5c9d2da82046234
japanessential <lama.nawraj00@gmail.com>
2023-12-11 23:58:10 +0900

    add feature for form validation frontend side with simple email and message

--- script.js
+++ script.js
@@ -20,3 +20,56 @@ document.addEventListener("DOMContentLoaded",function(){
         });
     }
 });
+
+
+//form validation
+document.addEventListener("DOMContentLoaded",function(){
+ const form = document.getElementById('contactForm');
+ form.addEventListener('submit',validateForm);
+
+ function validateForm(e){
+ e.preventDefault();
+
+ const nameInput = document.getElementById('name');
+ const emailInput= document.getElementById('email');
+ const messageInput = document.getElementById('message');
+
+ if(validateName(nameInput) && validateEmail(emailInput) && validateMessage(messageInput)){
+    //submit the form (you can add a ajax request here or let the form submit naturally.
+    alert('Form submitted successfully.');
+
+     }
+  }
+
+
+ function validateName(nameInput){
+ const nameValue = nameInput.value.trim();
+     if(nameValue ===''){
+        alert('Please enter your name.');
+        return false;
+     }
+     return true;
+ }
+
+ function validateEmail(emailInput){
+    const emailValue = emailInput.value.trim();
+
+    const emailRegex = /^[^¥s@]+@[¥s@]+¥.[^¥s@]+$/;
+
+    if(!emailRegex.test(emailValue)){
+        alert('Please enter a  valid email address.');
+        return false;
+    }
+    return true;
+ }
+
+ function validateMessage(messageInput){
+ const messageValue = messageInput.value.trim();
+    if(messageValue===''){
+    alert('Please enter a message.');
+        return false;
+    }
+    return true;
+ }
+
+})
